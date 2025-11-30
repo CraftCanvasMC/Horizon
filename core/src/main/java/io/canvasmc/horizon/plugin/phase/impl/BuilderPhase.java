bf368e10bb833a5eed2cf9eb3559b8c1fc1a0ea2
Dueris <jedimastertoothless@hotmail.com>
2025-11-29 18:27:06 -0800

    Simplify builder phase

--- core/src/main/java/io/canvasmc/horizon/plugin/phase/impl/BuilderPhase.java
+++ core/src/main/java/io/canvasmc/horizon/plugin/phase/impl/BuilderPhase.java
@@ -2,7 +2,6 @@ package io.canvasmc.horizon.plugin.phase.impl;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.stream.JsonReader;
 import io.canvasmc.horizon.Horizon;
 import io.canvasmc.horizon.plugin.LoadContext;
 import io.canvasmc.horizon.plugin.data.HorizonMetadata;
@@ -12,8 +11,6 @@ import io.canvasmc.horizon.plugin.types.HorizonPlugin;
 import io.canvasmc.horizon.plugin.types.PluginCandidate;
 import org.jspecify.annotations.NonNull;
 
-import java.io.IOException;
-import java.io.Reader;
 import java.util.*;
 
 public class BuilderPhase implements Phase<Set<PluginCandidate>, List<HorizonPlugin>> {
@@ -28,97 +25,10 @@ public class BuilderPhase implements Phase<Set<PluginCandidate>, List<HorizonPlu
 
         for (PluginCandidate candidate : input) {
             Map<String, Object> raw = candidate.metadata().rawData();
-            // TODO - simplify?
-            HorizonMetadata horizonMetadata = Horizon.GSON.fromJson(new JsonReader(new Reader() {
-                private String json = gson.toJson(raw);
-                private final int length = json.length();
-                private int next = 0;
-                private int mark = 0;
-
-                private void ensureOpen() throws IOException {
-                    if (json == null)
-                        throw new IOException("Stream closed");
-                }
-
-                @Override
-                public int read() throws IOException {
-                    synchronized (lock) {
-                        ensureOpen();
-                        if (next >= length)
-                            return -1;
-                        return json.charAt(next++);
-                    }
-                }
-
-                @Override
-                public int read(char @NonNull [] cbuf, int off, int len) throws IOException {
-                    synchronized (lock) {
-                        ensureOpen();
-                        Objects.checkFromIndexSize(off, len, cbuf.length);
-
-                        if (len == 0) return 0;
-                        if (next >= length) return -1;
-
-                        int n = Math.min(length - next, len);
-                        json.getChars(next, next + n, cbuf, off);
-                        next += n;
-                        return n;
-                    }
-                }
-
-                @Override
-                public long skip(long n) throws IOException {
-                    synchronized (lock) {
-                        ensureOpen();
-                        if (next >= length) return 0;
-
-                        long r = Math.min(n, (long) length - next);
-                        r = Math.max(r, -next);
-
-                        next += (int) r;
-                        return r;
-                    }
-                }
-
-                @Override
-                public boolean ready() throws IOException {
-                    synchronized (lock) {
-                        ensureOpen();
-                        return true;
-                    }
-                }
-
-                @Override
-                public boolean markSupported() {
-                    return true;
-                }
-
-                @Override
-                public void mark(int readAheadLimit) throws IOException {
-                    if (readAheadLimit < 0)
-                        throw new IllegalArgumentException("Read-ahead limit < 0");
-
-                    synchronized (lock) {
-                        ensureOpen();
-                        mark = next;
-                    }
-                }
-
-                @Override
-                public void reset() throws IOException {
-                    synchronized (lock) {
-                        ensureOpen();
-                        next = mark;
-                    }
-                }
-
-                @Override
-                public void close() {
-                    synchronized (lock) {
-                        json = null;
-                    }
-                }
-            }), HorizonMetadata.class);
+            HorizonMetadata horizonMetadata = Horizon.GSON.fromJson(
+                    gson.toJson(raw),
+                    HorizonMetadata.class
+            );
             if (horizonMetadata == null) {
                 throw new PhaseException("Couldn't deserialize horizon metadata for candidate '" + candidate.metadata().name() + "'");
             }
