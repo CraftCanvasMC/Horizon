05743be1db7cf54e47246a514a30c8b504a114d1
Dueris <jedimastertoothless@hotmail.com>
2025-12-09 13:21:48 -0800

    Set default priority of mixins to 1

--- core/src/main/java/io/canvasmc/horizon/inject/mixin/MinecraftServerMixin.java
+++ core/src/main/java/io/canvasmc/horizon/inject/mixin/MinecraftServerMixin.java
@@ -5,7 +5,7 @@ import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(value = MinecraftServer.class, priority = 1)
+@Mixin(value = MinecraftServer.class)
 public class MinecraftServerMixin {
 
     @ModifyReturnValue(method = "getServerModName", at = @At("RETURN"))
