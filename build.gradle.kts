524b78fdbe46ca4b6802ee29d597557a2ff85359
Toffikk <slodkitofik@gmail.com>
2025-12-09 18:27:42 +0100

    chore: fixup build scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,107 +12,110 @@ val PAPER_MAVEN = "https://repo.papermc.io/repository/maven-public/"
 val JDK_VERSION = 21
 
 allprojects {
-    apply(plugin = "java-library")
-    apply(plugin = "com.gradleup.shadow")
-    apply(plugin = "io.papermc.paperweight.userdev")
+    if (name != "kotlin-plugin") {
+        apply(plugin = "java-library")
+        apply(plugin = "com.gradleup.shadow")
+        apply(plugin = "io.papermc.paperweight.userdev")
 
-    repositories {
-        mavenCentral()
-        maven(PAPER_MAVEN)
-    }
-
-    // configuration that shades and includes as an implementation for the core project
-    val include by configurations.creating {
-        configurations.implementation.get().extendsFrom(this)
-    }
-
-    dependencies {
-        // general libraries
-        include("com.google.code.gson:gson:2.13.2")
-        include("org.yaml:snakeyaml:2.3")
-        include("com.google.guava:guava:33.4.0-jre")
-        include("net.sf.jopt-simple:jopt-simple:5.0.4")
-
-        // annotations -- compileOnly
-        compileOnly("org.jspecify:jspecify:1.0.0")
-
-        // logger libraries
-        implementation("org.tinylog:tinylog-api:2.7.0")
-        implementation("org.tinylog:tinylog-impl:2.7.0")
-
-        // asm
-        include("org.ow2.asm:asm:9.9")
-        include("org.ow2.asm:asm-analysis:9.9")
-        include("org.ow2.asm:asm-commons:9.9")
-        include("org.ow2.asm:asm-tree:9.9")
-        include("org.ow2.asm:asm-util:9.9")
-
-        // mixin libraries
-        implementation("net.fabricmc:sponge-mixin:0.16.5+mixin.0.8.7")
-        implementation("io.github.llamalad7:mixinextras-common:0.5.0")
-        implementation("net.fabricmc:access-widener:2.1.0")
-
-        // paperclip patching
-        include("io.sigpipe:jbsdiff:1.0")
-
-        // minecraft setup
-        paperweight.paperDevBundle("1.21.10-R0.1-SNAPSHOT")
-    }
-
-    tasks.shadowJar {
-        configurations = listOf(project.configurations.runtimeClasspath.get())
-
-        val basePackage = "horizon.libs"
-
-        // this is realistically a *rough* relocation. this won't relocate
-        // everything, though we can't have it relocate everything
-        include.resolvedConfiguration.resolvedArtifacts.forEach { artifact ->
-            val group = artifact.moduleVersion.id.group
-            val rootPackage = group.split(".").take(2).joinToString(".")
-
-            relocate(rootPackage, "$basePackage.$rootPackage")
+        repositories {
+            mavenCentral()
+            maven(PAPER_MAVEN)
         }
 
-        // relocate license files
-        eachFile {
-            if (path.endsWith("LICENSE.txt") || path.endsWith("LICENSE") ||
-                path.endsWith("NOTICE.txt") || path.endsWith("NOTICE")
-            ) {
-                path = "horizon/libs/licenses/$name"
+        // configuration that shades and includes as an implementation for the core project
+        val include by configurations.creating {
+            configurations.implementation.get().extendsFrom(this)
+        }
+
+        dependencies {
+            // general libraries
+            include("com.google.code.gson:gson:2.13.2")
+            include("org.yaml:snakeyaml:2.3")
+            include("com.google.guava:guava:33.4.0-jre")
+            include("net.sf.jopt-simple:jopt-simple:5.0.4")
+
+            // annotations -- compileOnly
+            compileOnly("org.jspecify:jspecify:1.0.0")
+
+            // logger libraries
+            implementation("org.tinylog:tinylog-api:2.7.0")
+            implementation("org.tinylog:tinylog-impl:2.7.0")
+
+            // asm
+            include("org.ow2.asm:asm:9.9")
+            include("org.ow2.asm:asm-analysis:9.9")
+            include("org.ow2.asm:asm-commons:9.9")
+            include("org.ow2.asm:asm-tree:9.9")
+            include("org.ow2.asm:asm-util:9.9")
+
+            // mixin libraries
+            implementation("net.fabricmc:sponge-mixin:0.16.5+mixin.0.8.7")
+            implementation("io.github.llamalad7:mixinextras-common:0.5.0")
+            implementation("net.fabricmc:access-widener:2.1.0")
+
+            // paperclip patching
+            include("io.sigpipe:jbsdiff:1.0")
+
+            // minecraft setup
+            paperweight.paperDevBundle("1.21.10-R0.1-SNAPSHOT")
+        }
+
+        tasks.shadowJar {
+            configurations = listOf(project.configurations.runtimeClasspath.get())
+
+            val basePackage = "horizon.libs"
+
+            // this is realistically a *rough* relocation. this won't relocate
+            // everything, though we can't have it relocate everything
+            include.resolvedConfiguration.resolvedArtifacts.forEach { artifact ->
+                val group = artifact.moduleVersion.id.group
+                val rootPackage = group.split(".").take(2).joinToString(".")
+
+                relocate(rootPackage, "$basePackage.$rootPackage")
+            }
+
+            // relocate license files
+            eachFile {
+                if (path.endsWith("LICENSE.txt") || path.endsWith("LICENSE") ||
+                    path.endsWith("NOTICE.txt") || path.endsWith("NOTICE")
+                ) {
+                    path = "horizon/libs/licenses/$name"
+                }
+            }
+
+            archiveClassifier.set("")
+
+            // configure manifest
+            val version = fetchVersion()
+            val main = project.properties["main-class"]
+            val launchAgent = project.properties["instrumentation"]
+            manifest {
+                attributes(
+                    mapOf(
+                        "Main-Class" to "$main",
+                        "Implementation-Title" to "Horizon",
+                        "Implementation-Version" to version,
+                        "Specification-Title" to "Horizon",
+                        "Specification-Version" to version,
+                        "Specification-Vendor" to "CanvasMC Team",
+                        "Brand-Id" to "canvasmc:horizon",
+                        // jvm agent
+                        "Launcher-Agent-Class" to "$launchAgent",
+                        "Can-Redefine-Classes" to true,
+                        "Can-Retransform-Classes" to true
+                    )
+                )
             }
         }
 
-        archiveClassifier.set("")
-
-        // configure manifest
-        val version = fetchVersion()
-        val main = project.properties["main-class"]
-        val launchAgent = project.properties["instrumentation"]
-        manifest {
-            attributes(
-                mapOf(
-                    "Main-Class" to "$main",
-                    "Implementation-Title" to "Horizon",
-                    "Implementation-Version" to version,
-                    "Specification-Title" to "Horizon",
-                    "Specification-Version" to version,
-                    "Specification-Vendor" to "CanvasMC Team",
-                    "Brand-Id" to "canvasmc:horizon",
-                    // jvm agent
-                    "Launcher-Agent-Class" to "$launchAgent",
-                    "Can-Redefine-Classes" to true,
-                    "Can-Retransform-Classes" to true
-                )
-            )
-        }
-    }
-
-    java {
-        toolchain {
-            languageVersion.set(JavaLanguageVersion.of(JDK_VERSION))
+        java {
+            toolchain {
+                languageVersion.set(JavaLanguageVersion.of(JDK_VERSION))
+            }
         }
     }
 }
+
 fun fetchVersion(): Provider<String> {
     // fetch build number from jenkins property, if not present
     // then we can assume it is a local version
